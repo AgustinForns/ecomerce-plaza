1340dde3a86774a92ef94a280ed601ca411244e5
AgustinForns <103077691+AgustinForns@users.noreply.github.com>
2022-08-21 16:03:02 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar/>
+
+      <div className='body'>
+
+      </div>
+
+      <div className='footer'>
+
+      </div>
+
     </div>
+   
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from "react"
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+export default function NavBar(){
+    return(
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+              
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Bienvenido a Plaza!
+            </Typography>
+            <Button color="inherit">Novedades</Button>
+            <Button color="inherit">Productos</Button>
+            <Button color="inherit">Ofertas</Button>
+            <Button startIcon={<ShoppingCartIcon color="action"/>}></Button>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+   
+    )
+}
